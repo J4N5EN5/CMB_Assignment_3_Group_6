24bfa6b78c276a37d90040aaac34b90f60aab37b
Jan Oesterle <ge25goc@mytum.de>
2023-02-19 05:26:09 +0100

    adjusted the code a bit

---
+++ analysis_task2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+measurement_path = 'measurement_{}'.format(49924586)
+
+
+def data_preprocessing(df):
+    df.drop(['af', 'proto', 'step', 'type', 'fw', 'mver', 'dst_name', 'stored_timestamp', 'size', 'lts', 'src_addr',
+             'from', 'result', 'Latitude', 'Longitude'], axis=1, inplace=True)
+    return df
+
+
+def pipeline_part1(df):
+    return None
+
+
+def pipeline_part2(df):
+    return None
+
+
+def pipeline_part3(df):
+    return None
+
+
+def main():
+    df_results = pd.read_csv(f"{measurement_path}/measurement_results.csv")
+    df_results_preprocessed = data_preprocessing(df_results)
+    pipeline_part1(df_results_preprocessed)
+    pipeline_part2(df_results_preprocessed)
+    pipeline_part3(df_results_preprocessed)
+
+
+if __name__ == '__main__':
+    main()
